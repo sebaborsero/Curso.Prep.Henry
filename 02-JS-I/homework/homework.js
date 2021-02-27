946a65730c26b3803d1e8939cb9f8908c3e7c209
Seba Borsero <sebastiaborsero@gmail.com.com>
2021-02-27 15:33:23 -0300

    git push origin master

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -60,13 +60,18 @@ function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
-  return 
+  if ( x===y) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
+  return str1.length === str2.length
 }
 
 function menosQueNoventa(num) {
